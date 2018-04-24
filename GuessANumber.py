bfd36bcdbecfddeecdfcc03158aebe0eb799064e
sisinga <sisinga@SISINGA-T460-2.us.oracle.com>
2018-04-24 18:09:24 -0500

    new commit msg

---
+++ GuessANumber.py
@@ -0,0 +1,13 @@
+'''
+Created on Apr 24, 2018
+
+@author: sisinga
+'''
+print ('Guess a number')
+a = input ('Please input a number')
+a = int (a)
+if a < 50:
+    print ('Your number is less than 50')
+else: print ('Your number is more than 50')
+
+ 
